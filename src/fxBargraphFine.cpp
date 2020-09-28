82fc3cf89a4a7d7e0019ed7a3818856afd5a4094
FeLIX <git@felixm.net>
2020-09-29 00:10:01 +0200

    Create subclasses for fine and highres variant

---
+++ src/fxBargraphFine.cpp
@@ -0,0 +1,18 @@
+#include "fxBargraph.h"
+
+fxBargraphFine::fxBargraphFine(LiquidCrystal_I2C& lcd, byte x, byte y, byte id, byte width, int max, byte clr=' ', bool rtl=false)
+  : fxBargraph(lcd, x, y, id, width, max, clr, rtl) {
+    steps = CHAR_COLS + 1; // bridge inter-character gap
+}
+
+void fxBargraphFine::createFraction(byte frac) {
+  // Create fractional custom character
+  byte dyn[CHAR_ROWS];
+  byte x;
+  if (rtl) x = (0b11111100 << frac) >> 8;
+  else x = 0b11111000000 >> frac;
+  for (int row=0; row<CHAR_ROWS; row++)
+      dyn[row] = x;
+  lcd.createChar(id, dyn);
+}
+
---
+++ src/fxBargraphHighres.cpp
@@ -0,0 +1,18 @@
+#include "fxBargraph.h"
+
+fxBargraphHighres::fxBargraphHighres(LiquidCrystal_I2C& lcd, byte x, byte y, byte id, byte width, int max, byte clr=' ')
+  : fxBargraph(lcd, x, y, id, width, max, clr) {
+    steps = CHAR_COLS*CHAR_ROWS;
+}
+
+void fxBargraphHighres::createFraction(byte frac) {
+  // Create fractional custom character
+  byte dyn[CHAR_ROWS] = { 0,0,0,0,0,0,0,0 };
+  for (int row=0; row<CHAR_ROWS; row++)
+    for (int col=0; col<CHAR_COLS; col++) {
+      if (rtl) dyn[row] += (frac > (row+col*CHAR_ROWS)) ? 0x01 << col : 0x00;
+      else dyn[row] += (frac > (row+col*CHAR_ROWS)) ? 0x10 >> col : 0x00;
+    }
+  lcd.createChar(id, dyn);
+}
+
